beadd9b054d480b9f343df54decf512d3cb0f657
Trinh To <91444313+TrinhBTo@users.noreply.github.com>
2022-10-02 20:53:12 -0700

    new

---
+++ game_over_screen_with_restart_button/Assets/GameOver.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    public GameObject gameOverScreen;
+    bool gameOver = false;
+    int count = 0;
+
+    void Update()
+    {
+        if(gameOver)
+        {
+            gameOverScreen.SetActive(true);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (count==2){
+            gameOver = true;
+        }
+        count++;
+        
+    }
+
+    // Called when restart button is clicked
+    public void OnButtonClick()
+    {
+        SceneManager.LoadScene("Scene1");
+    }
+}
---
+++ text_follow_player/Assets/Show_Hide.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Show_Hide : MonoBehaviour
+{
+ 
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            this.GetComponent<Image>().enabled = !this.GetComponent<Image>().enabled;
+        }
+    }
+}
